006174d6ee87a167f5a8d2870f2db3e1b295bdeb
Jacob Duijzer <jacob@duijzer.com>
2022-07-07 15:17:16 +0200

    Added swagger.yaml

--- src/FirstBackstageDemoApi.Api/Program.cs
+++ src/FirstBackstageDemoApi.Api/Program.cs
@@ -1,25 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+app.UseSwagger();
+app.UseSwaggerUI(x => { x.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Zeipt Dashboard API"); });
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
